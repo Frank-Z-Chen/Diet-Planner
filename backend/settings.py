282b9252aed889da01fa5f7d2c460bfde4a81314
Zhaokuan Chen <zc56@illinois.edu>
2022-11-09 22:50:08 -0600

    Connect Django to Mysql

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proj1',
+        'HOST': '34.132.118.29',
+        'USER': 'root',
+        'PASSWORD': 'Q-team025-4846'
     }
 }
 
