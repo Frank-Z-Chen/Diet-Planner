eb22ed51c899fbab1469aaf11142eba8d6a40157
Zhaokuan Chen <zc56@illinois.edu>
2022-11-11 06:40:23 -0600

    Finish POST request

--- planner/serializers.py
+++ planner/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-
-class FoodSerializer(serializers.Serializer):
-    foodid = serializers.IntegerField() 
-    foodname = serializers.CharField(max_length=30) 
-    fat = serializers.FloatField()
-    protein = serializers.FloatField()
-    carb = serializers.FloatField()
+from planner.models import *
+# class FoodSerializer(serializers.Serializer):
+#     foodid = serializers.IntegerField() 
+#     foodname = serializers.CharField(max_length=30) 
+#     fat = serializers.FloatField()
+#     protein = serializers.FloatField()
+#     carb = serializers.FloatField()
+class FoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ['foodid', 'foodname', 'fat', 'protein', 'carb']
--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_list_or_404
 from django.db import connection
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,15 +13,23 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
-@api_view()
+@api_view(['GET','POST'])
 def show_foods(request):
     try:
-        sql = "SELECT * FROM Food"
-        qs = Food.objects.raw(sql)
-        qs_json = FoodSerializer(qs, many = True).data
-        if not qs_json:
-            raise Food.DoesNotExist
-        return Response(qs_json)
+        if request.method == 'GET':
+            qs = Food.objects.raw("SELECT * FROM Food")
+            serializer = FoodSerializer(qs, many = True)
+            if not serializer.data:
+                raise Food.DoesNotExist
+            return Response(serializer.data)
+        elif request.method == 'POST':
+            serializer = FoodSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            data=serializer.validated_data
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO Food VALUES(%s, %s, %s, %s, %s)", [data['foodid'], data['foodname'], data['fat'], data['protein'], data['carb']])  #these 3 lines can be directly replaced by serializer.save()
+            #serializer.save()　　　　　　　
+            return Response('OK')
     except Food.DoesNotExist:
         return Response(status=404)
 
@@ -32,8 +38,7 @@ def show_foods(request):
 def show_food_detail(request, id):
 
     try:
-        sql = "SELECT * FROM Food WHERE foodId = %s"
-        qs = Food.objects.raw(sql, [id])
+        qs = Food.objects.raw("SELECT * FROM Food WHERE foodId = %s", [id])
         qs_json = FoodSerializer(qs, many = True).data
         if not qs_json:
             raise Food.DoesNotExist
