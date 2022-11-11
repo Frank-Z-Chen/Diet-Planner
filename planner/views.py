5585ff8cf0ca430b1c72cb9208f09c6f7c4d7319
Zhaokuan Chen <zc56@illinois.edu>
2022-11-10 23:00:49 -0600

    Add HTTP 404 into Django

--- planner/views.py
+++ planner/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.db import connection
 from django.http import HttpResponse
-from planner.models import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from planner.models import *
 from .serializers import FoodSerializer
 
 def dictfetchall(cursor):
@@ -16,16 +17,36 @@ def dictfetchall(cursor):
 
 @api_view()
 def show_foods(request):
-    qs = Food.objects.raw("SELECT * FROM Food")
-    qs_json = FoodSerializer(qs, many = True).data
-    return Response(qs_json)
+    try:
+        sql = "SELECT * FROM Food"
+        qs = Food.objects.raw(sql)
+        qs_json = FoodSerializer(qs, many = True).data
+        if not qs_json:
+            raise Food.DoesNotExist
+        return Response(qs_json)
+    except Food.DoesNotExist:
+        return Response(status=404)
 
 
 @api_view()
 def show_food_detail(request, id):
-    qs = Food.objects.raw("SELECT * FROM Food WHERE foodId = %s", [id])
-    qs_json = FoodSerializer(qs, many = True).data
-    return Response(qs_json)
+    try:
+        sql = "SELECT * FROM Food WHERE foodId = %s"
+        qs = Food.objects.raw(sql, [id])
+        qs_json = FoodSerializer(qs, many = True).data
+        if not qs_json:
+            raise Food.DoesNotExist
+        return Response(qs_json)
+    except Food.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    # try:
+    #     food = Food.objects.get(pk=id)
+    #     serializer = FoodSerializer(food)
+    #     return Response(serializer.data)
+    # except Food.DoesNotExist:
+    #     return Response(status=404)
+
+    
 
 
 # @api_view()
