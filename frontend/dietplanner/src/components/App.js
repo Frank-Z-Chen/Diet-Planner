15922e836c7d85fd1dea5391de10b97519b00bcb
shadowjerry <370746534kjc@gmail.com>
2022-11-26 17:18:53 -0600

    upload user page and more

--- frontend/dietplanner/src/components/App.js
+++ frontend/dietplanner/src/components/App.js
@@ -4,18 +4,51 @@ import AdvQuery2 from "./AdvQuery2";
 import FoodForm from "./FoodForm";
 import FoodFormUpdate from "./FoodFormUpdate";
 import Home from "./Home";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import SignIn from "../signin";
+import NewUser from "../createUser";
+import HomePage from "../homePage";
+import Profile from "../profile";
+
+window.userName = "";
 
 class App extends React.Component {
 
   render(){
     return (
-      <div>
-        <Home />
-        <FoodForm />
-        <FoodFormUpdate />
-        <AdvQuery />
-        <AdvQuery2 />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path = '/'>
+            <div>
+              <SignIn />
+            </div>
+          </Route>
+          <Route exact path = '/createUser'>
+            <div>
+              <NewUser />
+            </div>
+          </Route>
+          <Route exact path = '/home'>
+            <div>
+              <HomePage />
+            </div>
+          </Route>
+          <Route exact path = '/profile'>
+            <div>
+              <Profile />
+            </div>
+          </Route>
+          <Route exact path = '/food'>
+            <div>
+              <Home />
+              <FoodForm />
+              <FoodFormUpdate />
+              <AdvQuery />
+              <AdvQuery2 />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 
