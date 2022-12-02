ebe418fe08a6fe3da93a951cb95027a6df85bb06
shadowjerry <370746534kjc@gmail.com>
2022-12-02 17:27:11 -0600

    profile editing finished

--- frontend/dietplanner/src/profile.js
+++ frontend/dietplanner/src/profile.js
@@ -5,11 +5,11 @@ import axios from "axios";
 const Profile = () => {
     const [userName, setuserName] = useState(window.userName);
     const [pwd, setPwd] = useState("");
-    const [email, setEmail] = useState(window.email);
+    //const [email, setEmail] = useState(window.email);
     const [age, setAge] = useState(window.age);
     const [gender, setGender] = useState(window.gender);
     const [calorieRecommand, setCalorieRecommand] = useState(window.calorieRecommand);
-    const [userId, setUserId] = useState(window.userId);
+    //const [userId, setUserId] = useState(window.userId);
     const [processing, setProcessing] = useState(false);
     const history = useHistory();
 
@@ -28,7 +28,7 @@ const Profile = () => {
         console.log(data);
 
         //vlidate user update
-        await axios.post('http://localhost:8000/planner/users/'+ window.userId +'/', data,{
+        await axios.patch('http://localhost:8000/planner/users/'+ window.userId +'/', data,{
             headers:{
                 'Authorization': window.token
             }
@@ -36,8 +36,9 @@ const Profile = () => {
         .then(res=>{
             console.log(res);
             console.log("Update finished");
-            getProfile();
-            console.log("recCal calculate finished");
+            history.push("/profile");
+            //getProfile();
+            //console.log("recCal calculate finished");
             setProcessing(false);
         })
         .catch(err =>{
@@ -62,9 +63,11 @@ const Profile = () => {
             }
             else{
                 //set the local value
-                setCalorieRecommand(res.data[0].recommend_cal);
                 window.calorieRecommand = res.data[0].recommend_cal;
-                
+                window.userName = res.data[0].userName;
+                window.email = res.data[0].email;
+                window.gender = res.data[0].gender;
+                window.age =res.data[0].age;
             }
         })
         .catch(err =>{
@@ -114,16 +117,6 @@ const Profile = () => {
                 />
                 <button>Submit Change</button>
             </form>
-            <h2>userName:</h2>
-            <p>{userName}</p>
-            <h2>email:</h2>
-            <p>{email}</p>
-            <h2>age:</h2>
-            <p>{age}</p>
-            <h2>gender:</h2>
-            <p>{gender}</p>
-            <h2>userId:</h2>
-            <p>{userId}</p>
             <h2>calorieRecommand:</h2>
             <p>{calorieRecommand}</p>
             <button onClick={returnHome}>Back to Home</button>
