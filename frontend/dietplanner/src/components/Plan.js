a69ce9619e1aace7b2d29757c0d2c9588e9e1fe3
szhan89 <zhangsiweiv@gmail.com>
2022-12-02 18:31:51 -0600

    add planUpdate

--- frontend/dietplanner/src/components/Plan.js
+++ frontend/dietplanner/src/components/Plan.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import request from "./request"
 import { useState, useEffect } from "react"
 import { Checkbox, Button,message ,Table} from 'antd';
+
 export default () => {
     const columns = [
         {
@@ -21,7 +22,8 @@ export default () => {
             dataIndex: 'total_carb',
         },
     ];
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [updatePlanId, setupdatePlanId] = useState("");
     useEffect(() => {
         setLoading(true);
         request.get('/planner/users/10000/recipes', 
@@ -51,6 +53,15 @@ export default () => {
             message.success('This is a success message');
         })
     }
+    const updateController = () => {
+         
+        request.patch('/planner/users/'+window.userId+'/plans/'+updatePlanId+'/', { userName: 'user1', recipeList: selectedRowKeys }).then(res => {
+            console.log(res)
+            message.success('This is a success message');
+        })
+    }
+
+
     const hasSelected = selectedRowKeys.length > 0;
     return (
         <div>
@@ -62,6 +73,20 @@ export default () => {
                 <Button type="primary" onClick={confirm} disabled={!hasSelected} loading={loading}>
                     confirm
                 </Button>
+                
+                <Button type="primary" onClick={updateController} disabled={!hasSelected} loading={loading}>
+                    UPDATE!
+                </Button>
+
+                <label>
+                    PlanId(UPDATE ONLY);
+                    <input
+                        type = "number"
+                        value = {updatePlanId}
+                        onChange = { (e) => {setupdatePlanId(e.target.value)}}
+                    />
+        </label>
+
             </div>
             <Table rowSelection={rowSelection} rowKey={record => record.recipeId} columns={columns} dataSource={data} />
         </div>
